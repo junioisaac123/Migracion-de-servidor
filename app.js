bc436cfdc5c130ece892c0538685c02dfa722ed4
Junior <junior197@gmail.com>
2023-08-03 08:27:21 -0500

    Agrega el proyecto con las mejoras del proyecto 3

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const app = express();
-
-const tasks = [
-  {
-    id: '123456',
-    isCompleted: false,
-    description: 'Walk the dog',
-  }
-];
-
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
-});
-
 const PORT = 3000;
+
+const listViewRouter = require('./list-view-router');
+const listEditRouter = require('./list-edit-router');
+
+// Middleware para validar métodos HTTP válidos
+const validateMethod = (req, res, next) => {
+  if (['GET', 'POST', 'PUT', 'DELETE'].includes(req.method)) {
+    next();
+  } else {
+    res.status(405).send('Método HTTP no permitido');
+  }
+};
+
+app.use(express.json());
+app.use(validateMethod);
+
+app.use('/list-view', listViewRouter);
+app.use('/list-edit', listEditRouter);
+
 app.listen(PORT, () => {
-  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
